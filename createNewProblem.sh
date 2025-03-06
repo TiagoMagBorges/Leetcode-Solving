b2a54183ae1cda5f781576c64dedc3ecd75341a3
TiagoMagBorges <tiagomagborges@gmail.com>
2025-03-06 16:25:39 -0300

    feat: created a bash to create the problem solving structure

---
+++ createNewProblem.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+  echo "Uso: ./createNewProblem.sh nomeDoProblema"
+  exit 1
+fi
+
+PROBLEM_NAME=$1
+SRC_DIR="src"
+PACKAGE_DIR="$SRC_DIR/$PROBLEM_NAME"
+
+# Cria o diretório do pacote
+mkdir -p "$PACKAGE_DIR"
+
+# Cria o arquivo Solution.java
+cat > "$PACKAGE_DIR/Solution.java" <<EOL
+package $PROBLEM_NAME;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
+EOL
+
+# Cria os arquivos de explicação
+touch "$PACKAGE_DIR/explanation.md"
+touch "$PACKAGE_DIR/explicacao.md"
+
+echo "Problema $PROBLEM_NAME criado com sucesso!"
---
+++ src/RemoveDuplicateFromSortedArrayII/Solution.java
@@ -0,0 +1,7 @@
+package RemoveDuplicateFromSortedArrayII;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+}
